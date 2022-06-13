c386f48b3991cbdc7a3e9183f7aa37b9eadf8c4a
Gimena Binaghi <gimenabinaghi@hotmail.com>
2022-06-13 11:14:39 -0300

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import { useState } from 'react';
 import Formulario from "./components/Formulario"
 import Hedaer from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
 
+  const [pacientes, setPacientes] = useState([]); //arranca como array vacio que se va llenando
+
   return (
     <div className="container mx-auto mt-20">
       <Hedaer /> 
 
       <div className="mt-12 md:flex">
-        <Formulario />
+        <Formulario 
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+        />
         <ListadoPacientes />
       </div>
 
